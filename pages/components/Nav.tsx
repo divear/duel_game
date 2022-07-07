bea5bd678e94407b98595989ead855733f91bae9
lukascobit <lukyn.odehnal333@gmail.com>
2022-07-07 20:13:17 +0200

    init

---
+++ pages/components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+	return (
+		<div>
+			<h1>Duel game</h1>
+		</div>
+	);
+}
+
+export default Nav;
---
+++ pages/game.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function game() {
+	return <div>game</div>;
+}
+
+export default game;
