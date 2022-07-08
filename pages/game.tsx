556d591d148174ba1830e8a854023e9abf6f7eff
lukascobit <lukyn.odehnal333@gmail.com>
2022-07-08 12:52:29 +0200

    canvas

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+let gcanvas;
 
 function game() {
-	return <div>game</div>;
+	const [score, setScore] = useState("");
+	let Xpos: number;
+	let Ypos: number;
+	let isDown = false;
+	const [lineWidth, setLineWidth] = useState(50);
+	const [color, setColor] = useState("black");
+	const [HEIGHT, setHEIGHT] = useState(0);
+	const [WIDTH, setWIDTH] = useState(0);
+
+	useEffect(() => {
+		setHEIGHT(
+			window.innerWidth > 820
+				? window.innerHeight / 1.2
+				: window.innerWidth
+		);
+		setWIDTH(
+			window.innerWidth > 820
+				? window.innerWidth / 1.1
+				: window.innerWidth
+		);
+		onmousedown = function (e) {
+			isDown = true;
+			this.onmousemove = (e) => {
+				if (isDown) {
+					Xpos = e.offsetX;
+					Ypos = e.offsetY;
+				}
+
+				this.onmouseup = () => {
+					isDown = false;
+				};
+			};
+		};
+		if (window.innerWidth < 820) {
+			window.ontouchmove = function (e) {
+				console.log(WIDTH);
+
+				Xpos = e.touches[0].clientX;
+				Ypos = e.touches[0].clientY - HEIGHT;
+			};
+		}
+	});
+
+	const canvas: any = useRef(null);
+
+	function Draw() {
+		const c = canvas.current && canvas.current.getContext("2d");
+		gcanvas = canvas.current;
+
+		if (c) {
+			c.fillStyle = color;
+			c.beginPath();
+			c.arc(Xpos, Ypos, lineWidth, 0, 2 * Math.PI);
+			c.fill();
+		}
+	}
+	return (
+		<div>
+			Score: {score}
+			<canvas
+				id="canvas"
+				onMouseMove={Draw}
+				onTouchMove={Draw}
+				ref={canvas}
+				width={WIDTH}
+				height={HEIGHT}
+			/>
+		</div>
+	);
 }
 
 export default game;
